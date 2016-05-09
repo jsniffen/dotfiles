885bdab6fbafbab54475d2e98059765e8f2584b8
U-BOC\sniff002 <julian.sniffen@gmail.com>
2016-05-09 08:31:13 -0400

    added path retention to .tmux.conf and fixed install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
+echo "Removingi the old .config folder from $olddir if it exists"
+cd $olddir
+rm -rf .config
+echo "...done"
+
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
@@ -13,12 +18,13 @@ echo "...done"
 echo "Moving the existing .config folder to $olddir"
 mv ~/.config $olddir
 echo "...done"
-echo "Moving the new .config folder to ~"
-mv $dir/.config ~
+echo "Copying the new .config folder to ~"
+
+cp $dir/.config ~
 echo "...done"
 
 for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
+	echo "Moving old $file from ~ to $olddir"
 	mv ~/$file $olddir
 	echo "Creating symlink to $file in home directory"
 	ln -s $dir/$file ~/$file
